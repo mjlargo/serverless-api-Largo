91dd27f4aa766fad85cd786e10da8363e7ac8719
mjlargo <mjlargo015@gmail.com>
2024-04-21 14:39:13 +0800

    Initial commit

---
+++ functions/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const router = require('./routes/author');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+    // your mongoDB Cloud URL
+const dbCloudUrl =
+    'mongodb+srv://<username>:<password>@<cluster>/<database>?retryWrites=true&w=majority';
+    // your mongoDB Cloud URL
+const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose
+    .connect(dbCloudUrl || dbLocalUrl)
+    .then(() => console.log('Connected to MongoDB')) 
+    .catch((error) => console.error('Failed to connect to MongoDB', error));
+
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
